ba8dd838c52988e8b89669e127bb9db424112b9f
cwirz <cwirz@pyango.ch>
2021-02-20 18:43:39 +0100

    Merge branch 'master' of /Users/colinwirz/Documents/Development/PyCharmGroupSetup/neat/mlp with conflicts.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
-    name='Hello world app',
+    name='A neat app ;)',
     ext_modules=cythonize("./entities/*.py"),
     zip_safe=False,
 )
